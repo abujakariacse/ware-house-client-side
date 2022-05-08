c15ac97945e52c49c86dc9b889df030c41655922
abujakariacse <abujakaria316@gmail.com>
2022-05-08 21:47:11 +0600

    blogs another qna added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -33,7 +33,7 @@ const Blogs = () => {
                         <p>2. These databases have fixed or static or predefined schema</p>
                         <p>3. These databases are not suited for hierarchical data storage.</p>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="col-lg-6 ">
                         <h5 className='text-center'>NoSQL</h5>
                         <p>1. Non-relational or distributed database system.</p>
                         <p>2. They have dynamic schema</p>
@@ -41,7 +41,7 @@ const Blogs = () => {
                     </div>
                 </div>
             </div>
-            <div className='container mt-5 d-flex justify-content-center align-items-center flex-column'>
+            <div className='container mt-5 d-flex justify-content-center align-items-center flex-column shadow p-3 mb-5 bg-white rounded'>
                 <h4>3. What is the purpose of jwt and how does it work .?</h4>
                 <div className='mt-5 w-50'>
                     <p>JWT (JSON Web Token) is an open standard used to share security information between two parties — a client and a server. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.</p>
